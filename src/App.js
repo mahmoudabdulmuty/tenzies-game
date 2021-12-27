b7fe393738a9690cc782790ee52fd9dfcc376068
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-27 20:49:09 +0200

    Hold Dice part 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
+import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import Die from './components/Die';
+
 export default function App() {
-	const [randomArrOfNums, setRandomArrOfNums] = useState(
-		generateRandomArrNums()
-	);
+	const [dice, setDice] = useState(generateRandomArrNums());
 	function generateRandomArrNums() {
 		const arrOfRandomNums = [];
 		for (let i = 0; i < 10; i++) {
 			const min = 1;
 			const max = 6;
 			const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-			arrOfRandomNums.push(randomNum);
+			arrOfRandomNums.push({
+				value: randomNum,
+				isHeld: true,
+				id: nanoid()
+			});
 		}
 		return arrOfRandomNums;
 	}
 	const rollDice = () => {
-		setRandomArrOfNums(generateRandomArrNums());
+		setDice(generateRandomArrNums());
+	};
+	const holdDice = (id) => {
+		console.log(id);
 	};
 	return (
 		<div className="container">
 			<main className="main">
 				<div className="dice-container">
-					{randomArrOfNums.map((randomNum, index) => (
-						<Die key={index} value={randomNum} />
+					{dice.map((randomNum) => (
+						<Die
+							isHeld={() => holdDice(randomNum.id)}
+							key={randomNum.id}
+							value={randomNum.value}
+						/>
 					))}
 				</div>
 				<button onClick={rollDice} className="roll-dice">
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,9 @@
 export default function Die(props) {
+	const styles = {
+		backgroundColor: props.isHeld ? '#59E391' : '#fff'
+	};
 	return (
-		<div className="die-face">
+		<div onClick={props.isHeld} style={styles} className="die-face">
 			<h2 className="die-num">{props.value}</h2>
 		</div>
 	);
