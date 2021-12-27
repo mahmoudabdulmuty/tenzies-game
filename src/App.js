3f74c47342256a65f24bffac167b5c9b76a7058a
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-27 22:11:10 +0200

    Roll Dice Part 3

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import Die from './components/Die';
 
 export default function App() {
 	const [dice, setDice] = useState(generateRandomArrNums());
+
+	function generateNewDie() {
+		const min = 1;
+		const max = 6;
+		const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+		return {
+			value: randomNum,
+			isHeld: false,
+			id: nanoid()
+		};
+	}
+
 	function generateRandomArrNums() {
 		const arrOfRandomNums = [];
 		for (let i = 0; i < 10; i++) {
-			const min = 1;
-			const max = 6;
-			const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-			arrOfRandomNums.push({
-				value: randomNum,
-				isHeld: false,
-				id: nanoid()
-			});
+			arrOfRandomNums.push(generateNewDie());
 		}
 		return arrOfRandomNums;
 	}
 	const rollDice = () => {
-		setDice(generateRandomArrNums());
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.isHeld ? die : generateNewDie();
+			})
+		);
 	};
 	const holdDice = (id) => {
 		setDice((oldDice) =>
@@ -31,6 +40,11 @@ export default function App() {
 	return (
 		<div className="container">
 			<main className="main">
+				<h1 className="title">Tenzies</h1>
+				<p className="instructions">
+					Roll until all dice are the same. Click each die to freeze it at its
+					current value between rolls.
+				</p>
 				<div className="dice-container">
 					{dice.map((randomNum) => (
 						<Die
