ea1eb244c1f63b524e42a4c936c766c2c6c50f63
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-27 21:56:31 +0200

    Hold Dice Part 2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
 			const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
 			arrOfRandomNums.push({
 				value: randomNum,
-				isHeld: true,
+				isHeld: false,
 				id: nanoid()
 			});
 		}
@@ -22,7 +22,11 @@ export default function App() {
 		setDice(generateRandomArrNums());
 	};
 	const holdDice = (id) => {
-		console.log(id);
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+			})
+		);
 	};
 	return (
 		<div className="container">
@@ -30,9 +34,10 @@ export default function App() {
 				<div className="dice-container">
 					{dice.map((randomNum) => (
 						<Die
-							isHeld={() => holdDice(randomNum.id)}
 							key={randomNum.id}
 							value={randomNum.value}
+							isHeld={randomNum.isHeld}
+							holdDice={() => holdDice(randomNum.id)}
 						/>
 					))}
 				</div>
--- src/components/Die.js
+++ src/components/Die.js
@@ -3,7 +3,7 @@ export default function Die(props) {
 		backgroundColor: props.isHeld ? '#59E391' : '#fff'
 	};
 	return (
-		<div onClick={props.isHeld} style={styles} className="die-face">
+		<div onClick={props.holdDice} style={styles} className="die-face">
 			<h2 className="die-num">{props.value}</h2>
 		</div>
 	);
