599a887cb0335b5139471f47cb6f650a5d47afc7
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-27 19:19:14 +0200

    Map array to Die components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+import Die from './components/Die';
 export default function App() {
+	const [randomArrOfNums] = useState(generateRandomArrNums());
+
+	function generateRandomArrNums() {
+		const arrOfRandomNums = [];
+		for (let i = 0; i < 10; i++) {
+			const min = 1;
+			const max = 6;
+			const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+			arrOfRandomNums.push(randomNum);
+		}
+		return arrOfRandomNums;
+	}
 	return (
-		<main className="main">
-			<div class="container"></div>
-		</main>
+		<div className="container">
+			<main className="main">
+				<div className="dice-container">
+					{randomArrOfNums.map((randomNum, index) => (
+						<Die key={index} value={randomNum} />
+					))}
+				</div>
+			</main>
+		</div>
 	);
 }
